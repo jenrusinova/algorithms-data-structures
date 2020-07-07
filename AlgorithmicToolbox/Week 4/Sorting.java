201be8c453ca9b125f0cb1f5f3a2b46eca4449e1
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2020-07-06 22:40:16 -0700

    Algorithmic Toolbox

---
+++ AlgorithmicToolbox/Week 4/Sorting.java
@@ -0,0 +1,116 @@
+/*
+Task. To force the given implementation of the quick sort algorithm to efficiently process sequences with
+few unique elements, your goal is replace a 2-way partition with a 3-way partition. That is, your new
+partition procedure should partition the array into three parts: < 𝑥 part, = 𝑥 part, and > 𝑥 part.
+
+Input Format. The first line of the input contains an integer 𝑛. The next line contains a sequence of 𝑛
+integers 𝑎0, 𝑎1, . . . , 𝑎𝑛−1.
+
+Constraints. 1 ≤ 𝑛 ≤ 10^5; 1 ≤ 𝑎𝑖 ≤ 10^9 for all 0 ≤ 𝑖 < 𝑛.
+
+Output Format. Output this sequence sorted in non-decreasing order.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+public class Sorting {
+    private static Random random = new Random();
+
+    private static int[] partition3(int[] a, int l, int r) {
+        int m1 = l;
+        int m2 = l;
+        int pivot = a[l];
+        for (int j = l + 1; j <= r; j++) {
+            if (a[j] > pivot) continue;
+            if (a[j] < pivot) {
+                swap(a, m1, j);
+                m1++;
+            }
+            swap(a, m2 + 1, j);
+            m2++;
+        }
+        int[] m = {m1, m2};
+        return m;
+    }
+    private static void swap(int[] a, int i, int j) {
+        int tmp = a[i]; a[i] = a[j]; a[j] = tmp;
+    }
+
+    private static int partition2(int[] a, int l, int r) {
+        int x = a[l];
+        int j = l;
+        for (int i = l + 1; i <= r; i++) {
+            if (a[i] <= x) {
+                j++;
+                int t = a[i];
+                a[i] = a[j];
+                a[j] = t;
+            }
+        }
+        int t = a[l];
+        a[l] = a[j];
+        a[j] = t;
+        return j;
+    }
+
+    private static void randomizedQuickSort(int[] a, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+        int k = random.nextInt(r - l + 1) + l;
+        int t = a[l];
+        a[l] = a[k];
+        a[k] = t;
+        //use partition3
+        int[] ms = partition3(a, l, r);
+        int m1 = ms[0], m2 = ms[1];
+        randomizedQuickSort(a, l, m1 - 1);
+        randomizedQuickSort(a, m2 + 1, r);
+    }
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        randomizedQuickSort(a, 0, n - 1);
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
